81f4dc1a3f50c2cbcc757387141df68f7fc95494
ol4juwon <olajuwonlawal2012@gmail.com>
2022-11-06 20:35:09 +0100

    dockerized

--- comments/index.js
+++ comments/index.js
@@ -17,7 +17,7 @@ app.post("/posts/:id/comments", (req, res) => {
    
     const id = randomBytes(10).toString("hex");
     const {comment} = req.body;
-    const data  = {id, comments:comment};
+    const data  = {id, comments:comment, status: 'pending'};
     const postId = req.params.id;
     comments[postId] = comments[postId] || [];
     comments[postId].push(data);
@@ -31,7 +31,26 @@ app.post("/posts/:id/comments", (req, res) => {
 });
 
 app.post("/events", async (req, res) => {
-    console.log("Received Event", req.body.type);
+    const {type, data} = req.body;
+    console.log("Event Received:", type);
+
+    if ( type === 'CommentModerated') {
+        const {postId, id, status ,comments: comms} = data;
+        const commentss = comments[postId];
+        const comment = commentss.find(comment => comment.id === id);
+        comment.status = status;
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentUpdated",
+            data: {
+                id,
+                postId,
+                comments:comms,
+                status
+            }
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
     res.send({});
 });
 app.listen(4001, () => {
